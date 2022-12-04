82a456cd459ec064a515d0c933b692c4d32457e3
Maanu Grover <19lion@gmail.com>
2022-12-03 22:17:35 -0600

    implement feedforward

--- basicnn/network.py
+++ basicnn/network.py
@@ -1,11 +1,33 @@
 import numpy as np
 
 
+# Math Functions
+
+
+def sigmoid(x):
+    return 1/(1 + np.e ** -x)
+
+
 class Network:
+
     def __init__(self, layer_sizes):
-        self.wabs = []
+        self._wabs = []
         self.input_layer_size = layer_sizes[0]
         self.output_layer_size = layer_sizes[-1]
         for i in range(len(layer_sizes) - 1):
             weights = np.random.rand(layer_sizes[i + 1], layer_sizes[i] + 1)
-            self.wabs.append(weights)
+            self._wabs.append(weights)
+
+    def feedforward(self, input_acts):
+        if len(input_acts) != self.input_layer_size:
+            err = "Input activations should match input layer size of "
+            raise ValueError(err + str(self.input_layer_size))
+
+        acts = [np.array(input_acts)]
+        for layer in self._wabs:
+            weights = layer[:, :-1]
+            biases = layer[:, -1]
+            next_acts = sigmoid(weights.dot(acts[-1]) + biases)
+            acts.append(next_acts)
+
+        return acts
